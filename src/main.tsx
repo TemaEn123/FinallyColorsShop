121f5837469cb5ea18bf07327215561e0705a8d5
Artem Nechaev <ar.ne4aew@yandex.ru>
2024-07-29 16:28:08 +0300

    add redux api for colors and skeletons

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+
 import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./routes/AppRouter.tsx";
 
+import "./index.scss";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={AppRouter}></RouterProvider>
+  <Provider store={store}>
+    <RouterProvider router={AppRouter} />
+  </Provider>
 );
--- src/pages/SingleColor/SingleColor.tsx
+++ src/pages/SingleColor/SingleColor.tsx
@@ -1,5 +1,34 @@
+import { useParams } from "react-router-dom";
+import styles from "./styles.module.scss";
+import { useGetColorsByIdQuery } from "../../redux/services/colors";
+import Skeleton from "../../components/ui/Skeleton/Skeleton";
+import ColorItem from "../../components/ui/ColorItem/ColorItem";
+import { useEffect } from "react";
+
 const SingleColor = () => {
-  return <div>SingleColor</div>;
+  const params = useParams();
+
+  const {
+    data: color,
+    isLoading: isLoadingColor,
+    error: isErrorColor,
+  } = useGetColorsByIdQuery(params.id!);
+
+  useEffect(() => {
+    if (isErrorColor) {
+      throw new Error("Not Found");
+    }
+  }, [isErrorColor]);
+
+  return (
+    <section className={styles.color}>
+      {!isLoadingColor && color ? (
+        <ColorItem link={false} color={color} />
+      ) : (
+        <Skeleton count={1} />
+      )}
+    </section>
+  );
 };
 
 export default SingleColor;
